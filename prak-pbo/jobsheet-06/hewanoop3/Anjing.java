95a2a026f2e63cd4700ecbb133d369a65835e57f
Rifky <rifkypraditya1025@gmail.com>
2025-10-01 19:57:52 +0700

    percobaan 4

---
+++ prak-pbo/jobsheet-06/hewanoop3/Anjing.java
@@ -0,0 +1,11 @@
+package hewanoop3;
+
+class Anjing {
+  public Anjing(String nama, int umur) {
+    super(nama, umur);
+  }
+
+  void menggonggong() {
+    System.out.println("Guk...Guuk!");
+  }
+}
---
+++ prak-pbo/jobsheet-06/hewanoop3/Mamalia.java
@@ -0,0 +1,11 @@
+package hewanoop3;
+
+class Mamalia extends Hewan {
+  public Mamalia(String nama, int umur) {
+    super(nama, umur);
+  }
+
+  void menyusui() {
+    System.out.println("Menyusui anaknya...");
+  }
+}
